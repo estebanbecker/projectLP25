266db43a2f54fe82282b07da49332810f569a7c5
Esteban Becker <git@beckeresteban.eu>
2022-01-11 21:35:44 +0100

    Revert "Debug"
    This reverts commit c3774bb957f6c3afa5b997f6b0be399103c788c3.

--- query_exec.c
+++ query_exec.c
@@ -78,7 +78,8 @@ void execute_select(update_or_select_query_t *query) {
             &query->where_clause,
             record_list);
             
-    display_table_record_list(&record_list);
+    display_table_record_list(record_list);
+    free(record_list);
 }
 
 /*!
